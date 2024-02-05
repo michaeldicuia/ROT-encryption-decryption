33ca6ba68514a967fe3444d851589f42ee3ebb2a
Michael J. Dicuia <mjd480@grep.cs.rutgers.edu>
2024-02-04 19:27:07 -0500

    some message

---
+++ nai.c
@@ -0,0 +1,49 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]){
+
+	if (argc != 3) {printf("You must enter an encrypted string followed by the ROT scheme.\n"); return 1;}
+
+	int rotation = atoi(argv[2]);
+	int string_length = strlen(argv[1]);
+
+	//Makes an array of the given string, for easier iteration
+	char given_string[string_length];
+	for (int i=0; i<string_length; i++){  
+		given_string[i] = argv[1][i];
+	}
+
+//a = 97
+//A = 65
+//z = 122
+//Z = 90
+	char decoded[string_length+1]; decoded[string_length] = '\0';
+	char encoded[string_length+1]; encoded[string_length] = '\0';
+	for (int i=0; i<string_length; i++){
+//		if (given_string[i] == "'"){ encoded[i] = "'"; decoded[i] = "'";}
+		if ((given_string[i] > 96) && (given_string[i] < 123)){
+			encoded[i] = ((given_string[i] - 97 + rotation) % 26) + 97;
+			decoded[i] = ((given_string[i] - 97 + 26 - rotation) % 26) + 97;
+			continue; 
+		} 
+
+		if ((given_string[i] > 64) && (given_string[i] < 91)){
+			encoded[i] = ((given_string[i] - 65 + rotation) % 26) + 65;
+			decoded[i] = ((given_string[i] - 65 + 26 - rotation) % 26) + 65;
+			continue;
+		}
+		
+		encoded[i] = given_string[i];
+		decoded[i] = given_string[i];
+	} 
+	printf("The string to be coded is %s\n", given_string);
+
+printf("The encoded string is %s\n", encoded);
+printf("The decoded string is %s\n", decoded);
+
+
+}
